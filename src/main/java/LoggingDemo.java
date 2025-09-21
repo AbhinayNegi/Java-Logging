1107cdaf26914ba878bbeb0b34c8649b22385d9f
Abhiany <negia394@gmail.com>
2025-09-21 11:17:08 +0530

    Simple logging class added

--- src/main/java/LoggingDemo.java
+++ src/main/java/LoggingDemo.java
@@ -1,4 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LoggingDemo {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingDemo.class);
 
-
+    public static void main(String[] args){
+        logger.debug("This is debug");
+        logger.warn("This is warning log");
+        logger.error("This is error");
+        logger.info("This is info");
+    }
 }
